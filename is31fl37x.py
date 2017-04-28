ce1a59af9abfc205227afa3c49bbe2b54b3fa5bc
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2017-04-29 00:40:17 +0200

    Initial commit

---
+++ is31fl37x.py
@@ -0,0 +1,86 @@
+import utime
+
+
+class _Base:
+    def __init__(self, i2c, address=80):
+        self.address = address
+        self.i2c = i2c
+        self._i2c_buffer = bytearray(1)
+        self._brightness = 0
+        self._active = False
+
+    def _page(self, page):
+        self.i2c.writeto_mem(self.address, 0xfe, b'\xc5')
+        self._i2c_buffer[0] = page
+        self.i2c.writeto_mem(self.address, 0xfd, self._i2c_buffer)
+
+    def _config(self, register, value):
+        self._page(3)
+        self._i2c_buffer[0] = value
+        self.i2c.writeto_mem(self.address, register, self._i2c_buffer)
+
+    def active(self, value):
+        if value is None:
+            return self._active
+        self._config(0x00, 0x01 if value else 0x00)
+        self._active = bool(value)
+
+    def _pixel(self, x, y, color):
+        self._page(1)
+        self._i2c_buffer[0] = color
+        self.i2c.writeto_mem(self.address, x + y * 8, self._i2c_buffer)
+
+    def brightness(self, value=None):
+        if value is None:
+            return self._brightness
+        if not 0 <= value <= 255:
+            raise ValueError()
+        self._config(0x01, value)
+        self._brightness = value
+
+
+class Matrix8x8x2(_Base):
+    def __init__(self, i2c, address=80):
+        super().__init__(i2c, address)
+        self.active(1)
+        self.brightness(127)
+        self._page(0)
+        self.i2c.writeto_mem(self.address, 0, b'\xff' * 16 )
+
+    def pixel(self, x, y, color):
+        if not (0 <= x <= 7 and 0 <= y <= 7):
+            return
+        self._pixel(x, y * 2, color & 0xff)
+        self._pixel(7 - x, y * 2 + 1, (color >> 8) & 0xff)
+
+
+class PewPew(Matrix8x8x2):
+    def __init__(self, i2c, address=80):
+        super().__init__(i2c, address)
+        self._page(0)
+        self.i2c.writeto_mem(self.address, 0x12, b'\x3f')
+
+    def scan(self):
+        brightness = self.brightness()
+        self.brightness(1)
+        self._config(0x00, 0x05)
+        utime.sleep_ms(3)
+        self.brightness(brightness)
+        self._page(0)
+        buttons = self.i2c.readfrom_mem(self.address, 0x2a, 1)[0]
+        self._config(0x00, 0x01)
+        return buttons
+
+
+class Matrix7x11(_Base):
+    def __init__(self, i2c, address=80):
+        super().__init__(i2c, address)
+        self.active(1)
+        self.brightness(127)
+        self._page(0)
+        self.i2c.writeto_mem(self.address, 0, b'\x55\x15' * 11 )
+
+    def pixel(self, x, y, color):
+        if not (0 <= x <= 6 and 0 <= y <= 10):
+            return
+        self._pixel(x * 2, y * 2, color & 0xff)
